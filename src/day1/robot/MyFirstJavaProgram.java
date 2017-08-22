95fa83366bf87f2b91307a69e42660834842b223
Cole <league@WTS-IM-17.attlocal.net>
2017-08-22 11:56:17 -0700

    workshop day 2

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,21 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
-		
-		
-	}
+		System.out.println("hello world");
+		Robot friend =new Robot ("mini");
+		friend.penDown();
+		friend.setSpeed(10);
+		friend.setPenColor(0,255,0);
+		friend.turn(90);
+		friend.move(100);
+		friend.setPenColor(255,0,0);
+		friend.turn(90);
+        friend.move(100);
+        friend.setPenColor(0,0,255);
+        friend.turn(90);
+        friend.move(100);
+        friend.setPenColor(255,255,255);
+        friend.turn(90);
+        friend.move(100);
+	}   
 }
--- src/day1/robot/TriangleShell.java
+++ src/day1/robot/TriangleShell.java
@@ -5,7 +5,7 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
-
+	Robot friend =new Robot ("mini");
 	
 	void go() {
 		drawTriangle(100); // 3. delete this line (used only for testing)
@@ -28,7 +28,17 @@ public class TriangleShell {
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
 	private void drawTriangle(int length) {
-		
+		friend.setSpeed(10);
+		friend.setAngle(60);
+		friend.penDown();
+		friend.setPenColor(0,0,255);
+		friend.move(150);
+		friend.turn(120);
+		friend.setPenColor(255, 0, 0);
+		friend.move(150);
+		friend.setPenColor(0,255,0);
+		friend.turn(120);
+		friend.move(150);
 		
 		
 	}
