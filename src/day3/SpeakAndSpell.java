a5333f7f470aa1fd06e948c0add5311290117797
kamronraiszadeh <league@WTS19.attlocal.net>
2017-08-23 11:52:32 -0700

    day 3 workshop

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -14,15 +14,20 @@ import javax.swing.JOptionPane;
 public class SpeakAndSpell {
 
 	public static void main(String[] args) {
-		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
+		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"\
+		speak("how do you spell mandlebrot");
+		
 
 		// 2. Catch the user's answer in a String
-
+		
+String answer =JOptionPane.showInputDialog("please answer");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if(answer.equalsIgnoreCase("mandlebrot")) { answer= ("correct") ;}
 		// 4. Otherwise say "wrong"
 
-		// 5. repeat the process for other words
+		// 5. repeat the process for other world
+		
+      
 		
 	}
 
